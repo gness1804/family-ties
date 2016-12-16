1b2563761599d1e9e5d562acef8b0e8988846bc8
gness1804 <graham.nessler@gmail.com>
2016-12-15 18:08:00 -0700

    object is recognized by react

---
+++ lib/components/PersonCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+
+  render() {
+    return (
+      <div>
+        I am the person card. 
+      </div>
+    );
+  }
+
+}
+
+
+export default PersonCard;
